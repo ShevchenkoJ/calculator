fa23cbd83f6499c37e8f403efe4cd91ce3683724
ShevchenkoJ <shevchenko.juliia@gmail.com>
2020-01-28 01:36:59 +0200

    update calculator

--- script.js
+++ script.js
@@ -4,192 +4,174 @@
 // let result = 0;
 // console.log("result", result);
 
-function updateResult(argument1, argument2, operation) {
-
-    // console.log("event listener added");
-
-    // console.log(operationsArray, "let global");
+// function updateResult(argument1, argument2, operation) {
    
-    let originalResult = document.getElementById("result").value;
-    console.log(originalResult, "get original input value");
+//     let originalResult = document.getElementById("result").value;
+//     console.log(originalResult, "get original input value");
 
-    document.onclick = function(e){
-        let currentOperation = e.target.id;
-        //console.log(currentOperation, "was clicked");
+//     document.onclick = function(e) {
+//         let currentOperation = e.target.id;
 
-        switch (currentOperation) {
+//         switch (currentOperation) {
 
-            case "plus":
-                console.log("+");
-                document.getElementById("result").value = "";
+//             case "plus":
+//                 console.log("+");
+//                 document.getElementById("result").value = "";
 
-                document.getElementById("result").addEventListener('input', updateValueOnPlus);
+//                 document.getElementById("result").addEventListener('input', updateValueOnPlus);
 
-                function updateValueOnPlus(e) {
-                    //log.textContent = e.target.value;
-                    let updatedResult = document.getElementById("result").value;
-                    console.log(updatedResult, "get updated input value");
-                    let result = parseFloat(originalResult) + parseFloat(updatedResult);
-                    console.log(result, "result to show");
+//                 function updateValueOnPlus(e) {
+                    
+//                     let updatedResult = document.getElementById("result").value;
+//                     console.log(updatedResult, "get updated input value");
+//                     let result = parseFloat(originalResult) + parseFloat(updatedResult);
+//                     console.log(result, "result to show");
 
-                    let calculateButton = document.getElementById("calculate");
+//                     let calculateButton = document.getElementById("calculate");
                 
-                    calculateButton.addEventListener("click", showResult);
+//                     calculateButton.addEventListener("click", showResult);
 
-                    function showResult() {
-                        document.getElementById("result").value = result;
-                        console.log(result, "final result");
-                    }
-                }
+//                     function showResult() {
+//                         document.getElementById("result").value = result;
+//                         console.log(result, "final result");
+//                     }
+//                 }
 
-                // let calculateButton = document.getElementById("calculate");
+//               break;
+//             case "minus":
+//                 console.log("-");
+
+//                 document.getElementById("result").value = "";
+
+//                 document.getElementById("result").addEventListener('input', updateValueOnMinus);
+
+//                 function updateValueOnMinus(e) {
+                    
+//                     let updatedResult = document.getElementById("result").value;
+//                     console.log(updatedResult, "get updated input value");
+//                     let result = parseFloat(originalResult) - parseFloat(updatedResult);
+//                     console.log(result, "result to show");
+
+//                     let calculateButton = document.getElementById("calculate");
                 
-                // calculateButton.addEventListener("click", showResult);
-            
-                // function showResult() {
-                //     document.getElementById("result").value = result;
-                //     console.log(result, "final result");
-                // }
+//                     calculateButton.addEventListener("click", showResult);
 
-                // let result = originalResult + updatedResult;
-                // console.log(result, "result to show");
+//                     function showResult() {
+//                         document.getElementById("result").value = result;
+//                         console.log(result, "final result");
+//                     }
+//                 }
+
+//                 break;
+//             case "multiple":
+//               console.log("*");
+
+//               document.getElementById("result").value = "";
+
+//                 document.getElementById("result").addEventListener('input', updateValueOnMultiplication);
+
+//                 function updateValueOnMultiplication(e) {
+                
+//                     let updatedResult = document.getElementById("result").value;
+//                     console.log(updatedResult, "get updated input value");
+//                     let result = parseFloat(originalResult) * parseFloat(updatedResult);
+//                     console.log(result, "result to show");
+
+//                     let calculateButton = document.getElementById("calculate");
+                
+//                     calculateButton.addEventListener("click", showResult);
+
+//                     function showResult() {
+//                         document.getElementById("result").value = result;
+//                         console.log(result, "final result");
+//                     }
+//                 }
+
+//               break;
+//             case "divide":
+//               console.log("/");
+
+//               document.getElementById("result").value = "";
+
+//                 document.getElementById("result").addEventListener('input', updateValueOnDivision);
+
+//                 function updateValueOnDivision(e) {
+                    
+//                     let updatedResult = document.getElementById("result").value;
+//                     console.log(updatedResult, "get updated input value");
+//                     let result = parseFloat(originalResult) / parseFloat(updatedResult);
+//                     console.log(result, "result to show");
+
+//                     let calculateButton = document.getElementById("calculate");
+                
+//                     calculateButton.addEventListener("click", showResult);
+
+//                     function showResult() {
+//                         document.getElementById("result").value = result;
+//                         console.log(result, "final result");
+//                     }
+//                 }
+
+//               break;
           
-            //   let updatedResult = document.getElementById("result").value;
-            //   console.log(updatedResult, "get updated input value");
+//         }
 
-              //result = originalResult + updatedResult;
-              //console.log(result, "new result");
-              break;
-            case "minus":
-                console.log("-");
-
-                document.getElementById("result").value = "";
-
-                document.getElementById("result").addEventListener('input', updateValueOnMinus);
-
-                function updateValueOnMinus(e) {
-                    //log.textContent = e.target.value;
-                    let updatedResult = document.getElementById("result").value;
-                    console.log(updatedResult, "get updated input value");
-                    let result = parseFloat(originalResult) - parseFloat(updatedResult);
-                    console.log(result, "result to show");
-
-                    let calculateButton = document.getElementById("calculate");
-                
-                    calculateButton.addEventListener("click", showResult);
-
-                    function showResult() {
-                        document.getElementById("result").value = result;
-                        console.log(result, "final result");
-                    }
-                }
-
-                break;
-            case "multiple":
-              console.log("*");
-
-              document.getElementById("result").value = "";
-
-                document.getElementById("result").addEventListener('input', updateValueOnMultiplication);
-
-                function updateValueOnMultiplication(e) {
-                    //log.textContent = e.target.value;
-                    let updatedResult = document.getElementById("result").value;
-                    console.log(updatedResult, "get updated input value");
-                    let result = parseFloat(originalResult) * parseFloat(updatedResult);
-                    console.log(result, "result to show");
-
-                    let calculateButton = document.getElementById("calculate");
-                
-                    calculateButton.addEventListener("click", showResult);
-
-                    function showResult() {
-                        document.getElementById("result").value = result;
-                        console.log(result, "final result");
-                    }
-                }
-
-              break;
-            case "divide":
-              console.log("/");
-
-              document.getElementById("result").value = "";
-
-                document.getElementById("result").addEventListener('input', updateValueOnDivision);
-
-                function updateValueOnDivision(e) {
-                    //log.textContent = e.target.value;
-                    let updatedResult = document.getElementById("result").value;
-                    console.log(updatedResult, "get updated input value");
-                    let result = parseFloat(originalResult) / parseFloat(updatedResult);
-                    console.log(result, "result to show");
-
-                    let calculateButton = document.getElementById("calculate");
-                
-                    calculateButton.addEventListener("click", showResult);
-
-                    function showResult() {
-                        document.getElementById("result").value = result;
-                        console.log(result, "final result");
-                    }
-                }
-
-              break;
-            // case "calculate":
-            //     console.log("=");
-
-            //     // console.log(result, "result to show");
-            //     document.getElementById("result").value = parseFloat(result);
-            //     console.log(result, "result shown");
-
-            //     // function clickCalculateButton() {
-            //     //     let calculateButton = document.getElementById("calculate");
-            //     //     calculateButton.addEventListener("click", showResult);
-            //     // }
-                
-            //     // function showResult() {
-            //     //     document.getElementById("result").textContent = result;
-            //     //     console.log(result, "final result");
-            //     // }
-                
-            //     break; 
-        }
-
-    }
+//     }
         
-}
-
-let operationsArray = Array.from(document.getElementsByClassName("operation-button"));
-//console.log(operationsArray);
-let calculateButton = document.getElementById("calculate");
-//console.log(calculateButton, "calc button");
-
-// for (let i = 0; i < operationsArray.length; i++) { 
-//     //console.log(operationsArray[i].id);
-//     console.log(i);
-//     operationsArray[i].addEventListener("click", myFunc);
 // }
+
+const operationsArray = Array.from(document.getElementsByClassName("operation-button"));
+
+const calculateButton = document.getElementById("calculate");
+
+const valuesInput = document.getElementById("result");
+
+let result = 0;
+
+let operationType; 
 
 operationsArray.forEach(function(item, i) {
-    //console.log(operationsArray[i]);
-    operationsArray[i].addEventListener("click", updateResult);
+    operationsArray[i].addEventListener("click", operationButtonClick);
 });
 
-function clickCalculateButton() {
-    calculateButton.addEventListener("click", showResult);
+calculateButton.addEventListener("click", calculateButtonClick);
+
+function operationButtonClick(event) {
+    operationType = event.target.id;
+    result = valuesInput.value;
 }
 
-function showResult() {
-    document.getElementById("result").value = result;
-    console.log(result, "final result");
+
+function calculateButtonClick(event) {
+    console.log(operationCheck(operationType, result, valuesInput.value));
+ }
+
+function operationCheck(operation, argument1, argument2) {
+    let result = 0;
+    switch (operation) {
+        case "plus":
+            result = argument1 + argument2;
+            break;
+    
+        case "minus":
+            result = argument1 - argument2;
+            break;
+
+        case "multiple":
+            result = argument1 * argument2;
+            break;
+        
+        case "divide":
+            result = argument1 / argument2;
+            break;
+
+        default:
+            alert("press +, -, *, / or =");
+            break;
+    }
+    return result;
 }
 
-
-//myId.addEventListener("click", myFunc);
-
-// function myFunc() {
-//     console.log("event listener added");
-//     //rewrite result
-//     //clean input up
-// }
-
+function cleanValue() {
+    valuesInput.value = "";
+}
